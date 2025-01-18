3249fefedcc61e0c70e18d7d9e4f564e049f5092
Megan Liu <meganliu@megans-macbook.mynetworksettings.com>
2025-01-17 23:51:37 -0500

    did everyting up until 1.3.3

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,9 +1,13 @@
+import { Routes, Route, Navigate } from "react-router";
 import Account from "./Account";
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
       <h1>Kambaz</h1>
-      <Account />
+      <Routes>
+        <Route path="/" element={<Navigate to="Account" />} />
+        <Route path="/Account/*" element={<Account />} />
+      </Routes>
     </div>
   );
 }
