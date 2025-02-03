a6e1c3dc32ffd33f9702b308e549c384ae694e42
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-02-02 22:02:59 -0500

    finished everything up until 2.3.16

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -1,10 +1,15 @@
 import {
+  Button,
+  Col,
   Form,
   FormControl,
   FormGroup,
   FormLabel,
   FormSelect,
+  InputGroup,
+  Row,
 } from "react-bootstrap";
+import FormRange from "react-bootstrap/esm/FormRange";
 
 export default function BootstrapForms() {
   return (
@@ -57,6 +62,111 @@ export default function BootstrapForms() {
           id="custom-switch"
           label="Checked disabled switch checkbox input"
         />
+      </div>
+      <div id="wd-css-styling-range-and-sliders">
+        <h3>Range</h3>
+        <FormGroup controlId="wd-range1">
+          <FormLabel>Example range</FormLabel>
+          <FormRange min="0" max="5" step="0.5" />
+        </FormGroup>
+      </div>
+      <div id="wd-css-styling-addons">
+        <h3>Addons</h3>
+        <InputGroup className="mb-3">
+          <InputGroup.Text>$</InputGroup.Text>
+          <InputGroup.Text>0.00</InputGroup.Text>
+          <FormControl />
+        </InputGroup>
+        <InputGroup>
+          <FormControl />
+          <InputGroup.Text>$</InputGroup.Text>
+          <InputGroup.Text>0.00</InputGroup.Text>
+        </InputGroup>
+      </div>
+      <div id="wd-css-responsive-forms-1">
+        <h3>Responsive forms</h3>
+        <Form.Group as={Row} className="mb-3" controlId="email1">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="email" value="email@example.com" />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="password1">
+          <Form.Label column sm={2}>
+            Password
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="textarea2">
+          <Form.Label column sm={2}>
+            Bio
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control as="textarea" style={{ height: "100px" }} />
+          </Col>
+        </Form.Group>
+      </div>
+      <div id="wd-css-responsive-forms-2">
+        <h3>Responsive forms</h3>
+        <Form>
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm={2}>
+              {" "}
+              Email{" "}
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="email" placeholder="Email" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm={2}>
+              {" "}
+              Password{" "}
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+          <fieldset>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label as="legend" column sm={2}>
+                Radios{" "}
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="radio"
+                  label="first radio"
+                  checked
+                  name="formHorizontalRadios"
+                />
+                <Form.Check
+                  type="radio"
+                  label="second radio"
+                  name="formHorizontalRadios"
+                />
+                <Form.Check
+                  type="radio"
+                  label="third radio"
+                  name="formHorizontalRadios"
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Form.Check label="Remember me" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Col>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </Form.Group>
+        </Form>
       </div>
     </div>
   );
