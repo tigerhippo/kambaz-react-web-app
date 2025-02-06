94153d64750d62b98112bee46da3375421493558
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-02-06 02:35:25 -0500

    added id

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -19,7 +19,9 @@ export default function TOC() {
         <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://github.com/jannunzi">My GitHub</Nav.Link>
+        <Nav.Link id="wd-github" href="https://github.com/jannunzi">
+          My GitHub
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   );
