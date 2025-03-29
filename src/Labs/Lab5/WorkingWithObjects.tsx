171159660a550c7d236ef2823ac2acf0c5356631
Megan Liu <meganliu@Megans-MacBook.local>
2025-03-29 00:21:35 -0400

    did everything up until 5.2.4 except for 2nd half

---
+++ src/Labs/Lab5/WorkingWithObjects.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import FormControl from "react-bootstrap/esm/FormControl";
+
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+export default function WorkingWithObjects() {
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const ASSIGNMENT_API_URL = `${REMOTE_SERVER}/lab5/assignment`;
+
+  const [module, setModule] = useState({
+    id: 1,
+    name: "The Reagan Presidency",
+    description:
+      "Learning about the key events and figures that shaped the Reagan presidency",
+    course: "US History",
+  });
+  const MODULE_API_URL = `${REMOTE_SERVER}/lab5/module`;
+
+  return (
+    <div id="wd-working-with-objects">
+      <h3>Working With Objects</h3>
+      <h4>Modifying Properties</h4>
+      <a
+        id="wd-update-assignment-title"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/title/${assignment.title}`}
+      >
+        Update Assignment Title
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-title"
+        defaultValue={assignment.title}
+        onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })
+        }
+      />
+      <hr />
+      <h4>Retrieving Objects</h4>
+      <a
+        id="wd-retrieve-assignments"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/assignment`}
+      >
+        Get Assignment
+      </a>
+      <hr />
+      <h4>Retrieving Properties</h4>
+      <a
+        id="wd-retrieve-assignment-title"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/assignment/title`}
+      >
+        Get Assignment Title
+      </a>
+      <hr />
+      <h4>Modifying Properties Part 2</h4>
+      <a
+        id="wd-update-module-name"
+        className="btn btn-primary float-end"
+        href={`${MODULE_API_URL}/name/${module.name}`}
+      >
+        Update Module Name
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-module-name"
+        defaultValue={module.name}
+        onChange={(e) => setModule({ ...module, name: e.target.value })}
+      />
+      <hr />
+      <h4>Retrieving Objects Part 2</h4>
+      <a
+        id="wd-retrieve-modules"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/module`}
+      >
+        Get Module
+      </a>
+      <hr />
+      <h4>Retrieving Properties Part 2</h4>
+      <a
+        id="wd-retrieve-module-name"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/module/name`}
+      >
+        Get Module Name
+      </a>
+      <hr />
+    </div>
+  );
+}
