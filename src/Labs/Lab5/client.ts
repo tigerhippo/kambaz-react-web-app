7121f55ab073574fdda3ca2d09112a407d6777cf
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-03-30 20:52:27 -0400

    did everything up until 5.2.5.6 except f or 5.2.4.7

---
+++ src/Labs/Lab5/client.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+export const fetchWelcomeMessage = async () => {
+  const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
+  return response.data;
+};
+
+const ASSIGNMENT_API = `${REMOTE_SERVER}/lab5/assignment`;
+export const fetchAssignment = async () => {
+  const response = await axios.get(`${ASSIGNMENT_API}`);
+  return response.data;
+};
+export const updateTitle = async (title: string) => {
+  const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
+  return response.data;
+};
