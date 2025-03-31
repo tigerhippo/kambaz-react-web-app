2642cdd4b4cfa681d0c2fd0177663d3a1b9a5509
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-03-30 23:54:28 -0400

    did everything up to 5.3

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -9,6 +9,7 @@ import { FaTrash } from "react-icons/fa6";
 import FormControl from "react-bootstrap/esm/FormControl";
 export default function WorkingWithArraysAsynchronously() {
   const [todos, setTodos] = useState<any[]>([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const editTodo = (todo: any) => {
     const updatedTodos = todos.map((t) =>
@@ -17,8 +18,12 @@ export default function WorkingWithArraysAsynchronously() {
     setTodos(updatedTodos);
   };
   const updateTodo = async (todo: any) => {
-    await client.updateTodo(todo);
-    setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+    try {
+      await client.updateTodo(todo);
+      setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+    } catch (error: any) {
+      setErrorMessage(error.response.data.message);
+    }
   };
 
   const createTodo = async () => {
@@ -43,9 +48,14 @@ export default function WorkingWithArraysAsynchronously() {
     setTodos(updatedTodos);
   };
   const deleteTodo = async (todo: any) => {
-    await client.deleteTodo(todo);
-    const newTodos = todos.filter((t) => t.id !== todo.id);
-    setTodos(newTodos);
+    try {
+      await client.deleteTodo(todo);
+      const newTodos = todos.filter((t) => t.id !== todo.id);
+      setTodos(newTodos);
+    } catch (error: any) {
+      console.log(error);
+      setErrorMessage(error.response.data.message);
+    }
   };
 
   useEffect(() => {
@@ -55,6 +65,14 @@ export default function WorkingWithArraysAsynchronously() {
   return (
     <div id="wd-asynchronous-arrays">
       <h3>Working with Arrays Asynchronously</h3>
+      {errorMessage && (
+        <div
+          id="wd-todo-error-message"
+          className="alert alert-danger mb-2 mt-2"
+        >
+          {errorMessage}
+        </div>
+      )}
       <h4>
         Todos
         <FaPlusCircle
