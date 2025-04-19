bab8e9cc50352a4f30a08d90df5d70afd090d646
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-04-19 01:50:25 -0400

    did everything

--- src/Kambaz/Courses/Assignments/client.ts
+++ src/Kambaz/Courses/Assignments/client.ts
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
 export const findAssignmentById = async (assignmentId: string) => {
-  const response = await axios.get(`${ASSIGNMENTS_API}/${assignmentId}`);
+  const response = await axiosWithCredentials.get(
+    `${ASSIGNMENTS_API}/${assignmentId}`
+  );
   return response.data;
 };
 export const deleteAssignment = async (assignmentId: string) => {
-  const response = await axios.delete(`${ASSIGNMENTS_API}/${assignmentId}`);
+  const response = await axiosWithCredentials.delete(
+    `${ASSIGNMENTS_API}/${assignmentId}`
+  );
   return response.data;
 };
 export const updateAssignment = async (assignment: any) => {
-  const { data } = await axios.put(
+  const { data } = await axiosWithCredentials.put(
     `${ASSIGNMENTS_API}/${assignment._id}`,
     assignment
   );
--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -19,7 +19,9 @@ export const findModulesForCourse = async (courseId: string) => {
   return response.data;
 };
 export const findAssignmentsForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/assignments`);
+  const response = await axiosWithCredentials.get(
+    `${COURSES_API}/${courseId}/assignments`
+  );
   return response.data;
 };
 export const findUsersForCourse = async (courseId: string) => {
@@ -48,7 +50,7 @@ export const createAssignmentForCourse = async (
   courseId: string,
   assignment: any
 ) => {
-  const response = await axios.post(
+  const response = await axiosWithCredentials.post(
     `${COURSES_API}/${courseId}/assignments`,
     assignment
   );
